4cd8a5eb4af3405ba68451c6374850c7d449fac6
reissgrvs <rwag365@gmail.com>
2019-10-07 00:05:32 +0100

    Added memory functions for read and writing to byte, half and word. Working towards #13, only needs a publicly facing interface or full exposure

--- src/memory.cpp
+++ src/memory.cpp
@@ -43,3 +43,67 @@ Memory::Memory(std::string biosPath, std::string romPath)
 		infile.read(reinterpret_cast<char *>(mem.ext.rom.data()), length);
 	}
 }
+
+uint32_t Memory::ReadToSize(std::uint8_t* byte, AccessSize size)
+{
+	auto word = reinterpret_cast<std::uint32_t*>(byte);
+	return (*word) & size;
+}
+
+uint32_t Memory::Read(AccessSize size, std::uint32_t address, AccessType accessType)
+{
+	auto page = address >> 24;
+	//TODO: Take size into account
+	//TODO: Increment cycles based on AccessType
+	switch (page)
+	{
+	case 0x00: return ReadToSize(&mem.gen.bios[address & bios_mask], size);
+	case 0x01: return 0; //not used
+	case 0x02: return ReadToSize(&mem.gen.wramb[address & wramb_mask], size);
+	case 0x03: return ReadToSize(&mem.gen.wramc[address & wramc_mask], size);
+	case 0x04: return ReadToSize(&mem.gen.ioreg[address & ioreg_mask], size);
+	case 0x05: return ReadToSize(&mem.disp.pram[address & pram_mask], size);
+	case 0x06: return ReadToSize(&mem.disp.vram[address & vram_mask], size);
+	case 0x07: return ReadToSize(&mem.disp.oam[address & oam_mask], size);
+	case 0x08: case 0x09: 
+	case 0x0A: case 0x0B:
+	case 0x0C: case 0x0D:
+		return ReadToSize(&mem.ext.rom[address & rom_mask];
+	default:
+		return 0;
+	}
+}
+
+void Memory::WriteToSize(std::uint8_t* byte, std::uint32_t value, AccessSize size)
+{
+	switch (size)
+	{
+	case AccessSize::Byte:
+		(*byte) = value;
+		break;
+	case AccessSize::Half:
+		auto half = reinterpret_cast<std::uint16_t*>(byte);
+		(*half) = value;
+		break;
+	case AccessSize::Word:
+		auto word = reinterpret_cast<std::uint32_t*>(byte);
+		(*word) = value;
+		break;
+	}
+}
+
+void Memory::Write(AccessSize size, std::uint32_t address, std::uint32_t value, AccessType accessType)
+{
+	auto page = address >> 24;
+
+	switch (page)
+	{
+	case 0x02: WriteToSize(&mem.gen.wramb[address & wramb_mask], value, size); break;
+	case 0x03: WriteToSize(&mem.gen.wramc[address & wramc_mask], value, size); break;
+	case 0x04: WriteToSize(&mem.gen.ioreg[address & ioreg_mask], value, size); break;
+	case 0x05: WriteToSize(&mem.disp.pram[address & pram_mask], value, size); break;
+	case 0x06: WriteToSize(&mem.disp.vram[address & vram_mask], value, size); break;
+	case 0x07: WriteToSize(&mem.disp.oam[address & oam_mask], value, size); break;
+	default: break;
+	}
+}
--- src/memory.hpp
+++ src/memory.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "memory_regions.hpp"
+
 #include <array>
 #include <memory>
 #include <cstdint>
@@ -9,30 +11,48 @@ class Memory
 	public:
 		Memory(std::string biosPath, std::string romPath);
 		
+		enum AccessType
+		{
+			NSEQ,
+			SEQ
+		};
+
+		enum AccessSize
+		{
+			Byte = 0xFF,
+			Half = 0xFFFF,
+			Word = 0xFFFFFFFF
+		};
+		
+		uint32_t Read(AccessSize size, std::uint32_t address, AccessType type);
+		void Write(AccessSize size, std::uint32_t address, std::uint32_t value, AccessType type);
 	private:
+		uint32_t ReadToSize(std::uint8_t* byte, AccessSize size);
+		void WriteToSize(std::uint8_t* byte, std::uint32_t value, AccessSize size);
 
 		// https://problemkaputt.de/gbatek.htm#gbamemorymap
+		
 		struct MemoryMap 
 		{
-			struct GeneralInternal
+			struct
 			{
-				std::array<std::uint8_t, 0x04000> bios{};
-				std::array<std::uint8_t, 0x40000> wramb{};
-				std::array<std::uint8_t, 0x08000> wramc{};
-				std::array<std::uint8_t, 0x003FF> ioreg{};
+				std::array<std::uint8_t, bios_size> bios{};
+				std::array<std::uint8_t, wramb_size> wramb{};
+				std::array<std::uint8_t, wramc_size> wramc{};
+				std::array<std::uint8_t, ioreg_size> ioreg{};
 			} gen;
 
-			struct InternalDisplay
+			struct
 			{
-				std::array<std::uint8_t, 0x00400> pram{};
-				std::array<std::uint8_t, 0x18000> vram{};
-				std::array<std::uint8_t, 0x00400> oam{};
+				std::array<std::uint8_t, pram_size> pram{};
+				std::array<std::uint8_t, vram_size> vram{};
+				std::array<std::uint8_t, oam_size> oam{};
 			} disp;
 
-			struct External
+			struct
 			{
 				//TODO: Make this as small as possible when rom is loaded?
-				std::array<std::uint8_t, 0x2000000> rom{};
+				std::array<std::uint8_t, rom_size> rom{};
 			} ext;
 		} mem;
 };
