63a8c6a4ee72481d50a18f53491387137d10adaf
reissgrvs <rwag365@gmail.com>
2020-02-21 16:46:58 +0000

    Joystick support

--- src/joypad.cpp
+++ src/joypad.cpp
@@ -1,18 +1,19 @@
 #include "joypad.hpp"
 #include "utils.hpp"
 #include <SFML/Window/Keyboard.hpp>
+#include <SFML/Window/Joystick.hpp>
 
 void Joypad::keyUpdate() {
-  right = sf::Keyboard::isKeyPressed(sf::Keyboard::D);
-  left = sf::Keyboard::isKeyPressed(sf::Keyboard::A);
-  up = sf::Keyboard::isKeyPressed(sf::Keyboard::W);
-  down = sf::Keyboard::isKeyPressed(sf::Keyboard::S);
-  a = sf::Keyboard::isKeyPressed(sf::Keyboard::K);
-  b = sf::Keyboard::isKeyPressed(sf::Keyboard::L);
-  rightBump = sf::Keyboard::isKeyPressed(sf::Keyboard::I);
-  leftBump = sf::Keyboard::isKeyPressed(sf::Keyboard::O);
-  select = sf::Keyboard::isKeyPressed(sf::Keyboard::Num2);
-  start = sf::Keyboard::isKeyPressed(sf::Keyboard::Num1);
+  right = sf::Keyboard::isKeyPressed(sf::Keyboard::D) || (sf::Joystick::getAxisPosition(0, (sf::Joystick::Axis)6) > 60);
+  left = sf::Keyboard::isKeyPressed(sf::Keyboard::A) || (sf::Joystick::getAxisPosition(0, (sf::Joystick::Axis)6) < -60);
+  up = sf::Keyboard::isKeyPressed(sf::Keyboard::W)  || (sf::Joystick::getAxisPosition(0, (sf::Joystick::Axis)7) < -60);
+  down = sf::Keyboard::isKeyPressed(sf::Keyboard::S)  || (sf::Joystick::getAxisPosition(0, (sf::Joystick::Axis)7) > 60);
+  a = sf::Keyboard::isKeyPressed(sf::Keyboard::K) || sf::Joystick::isButtonPressed(0, 1);
+  b = sf::Keyboard::isKeyPressed(sf::Keyboard::L) || sf::Joystick::isButtonPressed(0, 0);
+  rightBump = sf::Keyboard::isKeyPressed(sf::Keyboard::I) || sf::Joystick::isButtonPressed(0, 5);
+  leftBump = sf::Keyboard::isKeyPressed(sf::Keyboard::O) || sf::Joystick::isButtonPressed(0, 4);
+  select = sf::Keyboard::isKeyPressed(sf::Keyboard::Num2) || sf::Joystick::isButtonPressed(0, 6);
+  start = sf::Keyboard::isKeyPressed(sf::Keyboard::Num1) || sf::Joystick::isButtonPressed(0, 7);
 
   bp = sf::Keyboard::isKeyPressed(sf::Keyboard::Space);
   esc = sf::Keyboard::isKeyPressed(sf::Keyboard::Escape);
